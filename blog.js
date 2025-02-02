d23444d4aebaaf4d99a4d46c6d4f0f649d531d5c
Arif <arifkhan408760@gmail.com>
2025-02-02 21:38:52 +0600

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,15 @@
+const blogposts = [
+  {
+    title: "Reunion Surveys v6 Launch",
+    description: "Announcing the launch of Reunion Surveys v6.",
+    content:
+      "We are excited to announce the launch of Reunion Surveys v6, a major update to our survey platform. This version includes a host of new features and improvements designed to make creating and managing surveys easier and more efficient. With a revamped user interface, enhanced analytics, and new customization options, Reunion Surveys v6 is the best version yet. We can't wait for you to try it out and see the difference it makes in your survey experience.",
+    date: "02 Feb 2025",
+    category: ["Product Updates", "Announcements"],
+    importance: 1,
+    image:
+      "https://i.ppy.sh/9b864944c40b034ac530654c97c9848db7eed380/68747470733a2f2f696d6775722d617263686976652e7070792e73682f646973636f72642f3934373835393938393531333531393133382d3935353134393235333932363333303431392d313030302e706e67",
+    author: "teamreunion",
+    authorlink: "./",
+  },
+];
