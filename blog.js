f8840ab572180c293c2883b9cd70f1907d5f1e03
Arif <arifkhan408760@gmail.com>
2025-02-06 21:13:16 +0600

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,4 +1,17 @@
 const blogposts = [
+   {
+    title: "Way to 100",
+    description: "We are so close to our first 100 members!",
+    content:
+      "Thanks to every single of those who played a role in order to gather these amazing members! A big shoutout to <span style='color: var(--brightest);'>Arijit Paul</sapn>, without him, this woudn't be possible. Many thanks to our Developer and ReunionTeam. There will be some perks for these first 100 members, they might get a badge called OG CREW! Stay tuned and make your renuions fun!",
+    date: "06 Feb 2025",
+    category: ["Announcements"],
+    importance: 1,
+    image:
+      "https://images2.alphacoders.com/855/thumb-1920-855531.png",
+    author: "teamreunion",
+    authorlink: "./",
+  },
   {
     title: "Reunion Surveys v6 Launch",
     description: "Announcing the launch of Reunion Surveys v6.",
@@ -6,7 +19,7 @@ const blogposts = [
       "We are excited to announce the launch of Reunion Surveys v6, a major update to our survey platform. This version includes a host of new features and improvements designed to make creating and managing surveys easier and more efficient. With a revamped user interface, enhanced analytics, and new customization options, Reunion Surveys v6 is the best version yet. We can't wait for you to try it out and see the difference it makes in your survey experience.",
     date: "02 Feb 2025",
     category: ["Product Updates", "Announcements"],
-    importance: 1,
+    importance: 2,
     image:
       "https://i.ppy.sh/9b864944c40b034ac530654c97c9848db7eed380/68747470733a2f2f696d6775722d617263686976652e7070792e73682f646973636f72642f3934373835393938393531333531393133382d3935353134393235333932363333303431392d313030302e706e67",
     author: "teamreunion",
