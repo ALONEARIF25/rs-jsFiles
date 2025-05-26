14d14baad44a92e479ae00f8aa123408a02cc53f
Arif <arifkhan408760@gmail.com>
2025-05-26 19:50:17 +0600

    blog

--- blog.js
+++ blog.js
@@ -1,19 +1,19 @@
 const blogposts = [
-    {
+  {
     title: "May 2025 Update",
     description: "Domain update, UI update, new pages and more!",
     content:
-      "After the Secondary School Certificate (SSC) exam of 2025, The founder and developer has implimented many new features along with new domain REUN.ORG. We added /slideshow, /search/[username] new UI, /join, /about etc. Member count is 104 till today; we are working hard to gather more members, we want to print magazine, but we need more users; since magazines are not free to pirnt; we will need to seek for sponsorships and donations. If you are interested you can contribute by donating small amounts! Till next time!",
-         date: "19 May 2025",
+      "After the Secondary School Certificate (SSC) exam of 2025, The founder and developer has implimented many new features along with new domain REUN.ORG. We added /slideshow, /search/[username] new UI, /join, /about etc. Member count is 104 till today; we are working hard to gather more members, we want to print magazine, but we need more users; since magazines are not free to pirnt; we will need to seek for sponsorships and donations. If you are interested you can contribute by donating small amounts! Till next time! Update: added shorter URL for search: reun.org/@username, please note this is not permanent link to your profile, permanent links do not include @, so your permanent link will be reun.org/username.",
+    date: "19 May 2025",
     category: ["Announcements"],
-        
+
     importance: 1,
     image:
       "https://img.freepik.com/free-vector/set-torii-gates-water_52683-44986.jpg",
     author: "alonearif",
     authorlink: "./",
   },
-   {
+  {
     title: "Way to 100",
     description: "We are so close to our first 100 members!",
     content:
@@ -21,8 +21,7 @@ const blogposts = [
     date: "06 Feb 2025",
     category: ["Announcements"],
     importance: 1,
-    image:
-      "https://images2.alphacoders.com/855/thumb-1920-855531.png",
+    image: "https://images2.alphacoders.com/855/thumb-1920-855531.png",
     author: "alonearif",
     authorlink: "./",
   },
