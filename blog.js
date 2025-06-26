bda810bf9f634e9732a200334cc2f0a353a4bc92
Arif <arifkhan408760@gmail.com>
2025-06-26 20:32:49 +0600

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,41 +1,36 @@
-const blogposts = [
+[
   {
-    title: "May 2025 Update",
-    description: "Domain update, UI update, new pages and more!",
-    content:
-      "After the Secondary School Certificate (SSC) exam of 2025, The founder and developer has implimented many new features along with new domain REUN.ORG. We added /slideshow, /search/[username] new UI, /join, /about etc. Member count is 104 till today; we are working hard to gather more members, we want to print magazine, but we need more users; since magazines are not free to pirnt; we will need to seek for sponsorships and donations. If you are interested you can contribute by donating small amounts! Till next time! Update: added shorter URL for search: reun.org/@username, please note this is not permanent link to your profile, permanent links do not include @, so your permanent link will be reun.org/username.",
-    date: "19 May 2025",
-    category: ["Announcements"],
+    "title": "May 2025 Update",
+    "description": "Domain update, UI update, new pages and more!",
+    "content": "After the Secondary School Certificate (SSC) exam of 2025, The founder and developer has implimented many new features along with new domain REUN.ORG. We added /slideshow, /search/[username] new UI, /join, /about etc. Member count is 104 till today; we are working hard to gather more members, we want to print magazine, but we need more users; since magazines are not free to pirnt; we will need to seek for sponsorships and donations. If you are interested you can contribute by donating small amounts! Till next time! Update: added shorter URL for search: reun.org/@username, please note this is not permanent link to your profile, permanent links do not include @, so your permanent link will be reun.org/username.",
+    "date": "19 May 2025",
+    "category": ["Announcements"],
 
-    importance: 1,
-    image:
-      "https://img.freepik.com/free-vector/set-torii-gates-water_52683-44986.jpg",
-    author: "alonearif",
-    authorlink: "./",
+    "importance": 1,
+    "image": "https://img.freepik.com/free-vector/set-torii-gates-water_52683-44986.jpg",
+    "author": "alonearif",
+    "authorlink": "./"
   },
   {
-    title: "Way to 100",
-    description: "We are so close to our first 100 members!",
-    content:
-      "Thanks to every single person who played a role in gathering these amazing members! A big shoutout to Arijit Paul—without him, this wouldn’t have been possible. Many thanks to our Developer and the Reunion Team. There will be some perks for the first 100 members—they might get a badge called OG CREW! Stay tuned and make your reunions fun!",
-    date: "06 Feb 2025",
-    category: ["Announcements"],
-    importance: 1,
-    image: "https://images2.alphacoders.com/855/thumb-1920-855531.png",
-    author: "alonearif",
-    authorlink: "./",
+    "title": "Way to 100",
+    "description": "We are so close to our first 100 members!",
+    "content": "Thanks to every single person who played a role in gathering these amazing members! A big shoutout to Arijit Paul—without him, this wouldn’t have been possible. Many thanks to our Developer and the Reunion Team. There will be some perks for the first 100 members—they might get a badge called OG CREW! Stay tuned and make your reunions fun!",
+    "date": "06 Feb 2025",
+    "category": ["Announcements"],
+    "importance": 1,
+    "image": "https://images2.alphacoders.com/855/thumb-1920-855531.png",
+    "author": "alonearif",
+    "authorlink": "./"
   },
   {
-    title: "Reunion Surveys v6 Launch",
-    description: "Announcing the launch of Reunion Surveys v6.",
-    content:
-      "We are excited to announce the launch of Reunion Surveys v6, a major update to our survey platform. This version includes a host of new features and improvements designed to make creating and managing surveys easier and more efficient. With a revamped user interface, enhanced analytics, and new customization options, Reunion Surveys v6 is the best version yet. We can't wait for you to try it out and see the difference it makes in your survey experience.",
-    date: "02 Feb 2025",
-    category: ["Product Updates", "Announcements"],
-    importance: 2,
-    image:
-      "https://i.ppy.sh/9b864944c40b034ac530654c97c9848db7eed380/68747470733a2f2f696d6775722d617263686976652e7070792e73682f646973636f72642f3934373835393938393531333531393133382d3935353134393235333932363333303431392d313030302e706e67",
-    author: "teamreunionai",
-    authorlink: "./",
-  },
-];
+    "title": "Reunion Surveys v6 Launch",
+    "description": "Announcing the launch of Reunion Surveys v6.",
+    "content": "We are excited to announce the launch of Reunion Surveys v6, a major update to our survey platform. This version includes a host of new features and improvements designed to make creating and managing surveys easier and more efficient. With a revamped user interface, enhanced analytics, and new customization options, Reunion Surveys v6 is the best version yet. We can't wait for you to try it out and see the difference it makes in your survey experience.",
+    "date": "02 Feb 2025",
+    "category": ["Product Updates", "Announcements"],
+    "importance": 2,
+    "image": "https://i.ppy.sh/9b864944c40b034ac530654c97c9848db7eed380/68747470733a2f2f696d6775722d617263686976652e7070792e73682f646973636f72642f3934373835393938393531333531393133382d3935353134393235333932363333303431392d313030302e706e67",
+    "author": "teamreunionai",
+    "authorlink": "./"
+  }
+]
